617e4d908419207029fbdd03123eaac405cbb661
Anna <usune@mail.ru>
2022-01-31 23:29:18 +0300

    исправлено

--- script.js
+++ script.js
@@ -1,75 +1,34 @@
-let num = 266219;
-console.log(typeof num);
+let lang = "en";
 
-let str = String(num);
-console.log(str); 
-
-console.log(str.length); 
-
-let a = 1;
-let i = 0;
-while (i<str.length) {
-a=a*Number(str[i]);  
-  i++; 
-}
-
-const result = a ** 3;
-
-let resul = String(result);
-console.log(resul.substr(0, 2)); 
-
-let lang ='ru';
+const arr1 = [
+  ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+  ["Mn", "Ts", "Wd", "Th", "Fr", "St", "Sn"],
+];
 
 if (lang == "ru") {
-  console.log('Понедельник');
-  console.log('Вторник');
-  console.log('Среда');
-  console.log('Четверг');
-  console.log('Пятница');
-  console.log('Суббота');
-  console.log('Воскресенье');
-
+  console.log(arr1[0]);
 } else if (lang == "en") {
-  console.log('Monday');
-  console.log('Tuesday');
-  console.log('Wednesday');
-  console.log('Thursday');
-  console.log('Friday');
-  console.log('Saturday');
-  console.log('Sunday');
-} 
-
-switch (lang) {
-  case 'ru':
-  console.log('Понедельник');
-  console.log('Вторник');
-  console.log('Среда');
-  console.log('Четверг');
-  console.log('Пятница');
-  console.log('Суббота');
-  console.log('Воскресенье');
-    break 
-  case 'en':
-  console.log('Monday');
-  console.log('Tuesday');
-  console.log('Wednesday');
-  console.log('Thursday');
-  console.log('Friday');
-  console.log('Saturday');
-  console.log('Sunday');
-    break 
+  console.log(arr1[1]);
 }
 
- let arr = {
-  'ru':['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
-  'en':['Mn', 'Ts', 'Wd', 'Th', 'Fr', 'St', 'Sn'],
-};
+switch (lang) {
+  case "ru":
+    console.log(arr1[0]);
+    break;
+  case "en":
+    console.log(arr1[1]);
+    break;
+}
 
+let arr = {
+  ru: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+  en: ["Mn", "Ts", "Wd", "Th", "Fr", "St", "Sn"],
+};
 console.log(arr[lang]);
 
-
-let = namePerson = "Антон";
-(namePerson === "Артем") ? console.log("директор") : (namePerson === "Александр") ? console.log("преподаватель") : console.log("студент");
-
-
-
+let namePerson = "Артем";
+namePerson === "Артем"
+  ? console.log("директор")
+  : namePerson === "Александр"
+  ? console.log("преподаватель")
+  : console.log("студент");
